8feac1414d760ab3ee4329bb26bdec69a76adc2d
newbi462 <isthisemailfree@gmail.com>
2019-12-16 15:38:12 -0500

    cannot be closed or opened if it is locked

--- src/dashboard/Dashboard.test.js
+++ src/dashboard/Dashboard.test.js
@@ -9,3 +9,21 @@ test('defaults to unlocked and open', () => {
   getByText(/unlocked/i);
   getByText(/open/i);
 });
+
+test('cannot be closed or opened if it is locked', () => {
+  const { getByText } = render(<Dashboard />);
+
+  //lock gate(close then lock)
+  const closeButton = getByText(/close gate/i);
+  fireEvent.click(closeButton);
+  const lockButton = getByText(/lock gate/i);
+  fireEvent.click(lockButton);
+
+  //try to open locked gate
+  const openButton = getByText(/open gate/i);
+  fireEvent.click(openButton);
+
+  //verify still closed
+  getByText(/closed/i);
+});
+//As it DEFULETS TO UNLOCKED how would you test cant be closed while locked from a user stand point?
